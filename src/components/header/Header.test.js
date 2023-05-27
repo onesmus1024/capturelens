96123bb3131b3ba16aaeee98d10ab86bd460f80f
Onesmus Wambugu <onesmus1024@gmail.com>
2023-05-27 13:54:52 +0300

    fix : header test suite

--- src/components/header/Header.test.js
+++ src/components/header/Header.test.js
@@ -2,20 +2,19 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Header from './Header';
 
-describe('Header', () => {
-    test('renders logo and navigation links', () => {
-        render(<Header />);
-        const logoElement = screen.getByText(/Logo/i);
-        expect(logoElement).toBeInTheDocument();
-        const homeLink = screen.getByText(/Home/i);
-        expect(homeLink).toBeInTheDocument();
-        const aboutLink = screen.getByText(/About/i);
-        expect(aboutLink).toBeInTheDocument();
-        const contactLink = screen.getByText(/Contact/i);
-        expect(contactLink).toBeInTheDocument();
-        const servicesLink = screen.getByText(/Services/i);
-        expect(servicesLink).toBeInTheDocument();
-        const loginLink = screen.getByText(/Login/i);
-        expect(loginLink).toBeInTheDocument();
-    });
+
+test('renders logo and navigation links', () => {
+    render(<Header />);
+    const logoElement = screen.getByText(/Logo/i);
+    expect(logoElement).toBeInTheDocument();
+    const homeLink = screen.getByText(/Home/i);
+    expect(homeLink).toBeInTheDocument();
+    const aboutLink = screen.getByText(/About/i);
+    expect(aboutLink).toBeInTheDocument();
+    const contactLink = screen.getByText(/Contact/i);
+    expect(contactLink).toBeInTheDocument();
+    const servicesLink = screen.getByText(/Services/i);
+    expect(servicesLink).toBeInTheDocument();
+    const loginLink = screen.getByText(/Login/i);
+    expect(loginLink).toBeInTheDocument();
 });
