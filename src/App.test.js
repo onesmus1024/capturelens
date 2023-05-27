2146f996e2001774d647cd56c48f4cfb6f056090
Onesmus Wambugu <onesmus1024@gmail.com>
2023-05-27 14:24:58 +0300

    fix : app  test suite

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,7 @@ import App from './App';
 describe('App', () => {
   test('renders header component', () => {
     render(<App />);
-    const headerElement = screen.getByText(/home/i);
+    const headerElement = screen.getByText(/about/i);
     expect(headerElement).toBeInTheDocument();
   });
 
