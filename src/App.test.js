1892f94b3672566f4470629c759a26bbc294d50a
Onesmus Wambugu <onesmus1024@gmail.com>
2023-05-27 13:40:46 +0300

    update  : added tailwind for css

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('renders header component', () => {
+    render(<App />);
+    const headerElement = screen.getByTestId('header');
+    expect(headerElement).toBeInTheDocument();
+  });
+
+  test('renders home route', () => {
+    render(<App />);
+    const homeRoute = screen.getByText(/home/i);
+    expect(homeRoute).toBeInTheDocument();
+  });
 });
